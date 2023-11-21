5d9cb237505f74c8861f96cf706d2efe45a70e37
Victormd37 <victor.molina.diez@estudiantat.upc.edu>
2023-11-21 10:30:53 +0100

    Primera versión del codigo de CBR (clases i métodos vacíos)

---
+++ CBR.py
@@ -0,0 +1,51 @@
+class CBR():
+
+    def __init__(self,case_db,domain):
+        self.cases = case_db #Base de Dades?
+        self.domain = domain #Arbre?
+
+    def retrieve(self, new_case):
+        pass
+
+    def reuse(self):
+        pass
+    
+    def revise(self):
+        pass
+
+    def retain(self):
+        pass
+
+    def _similarity(self,new_case):
+        pass
+
+class User():
+
+    def __init__(self,atributes):
+        self.a1 = atributes[0]
+        self.a2 = atributes[1]
+        self.a3 = atributes[2]
+        self.a4 = atributes[3]
+        self.a5 = atributes[4]
+
+class Book():
+
+    def __init__(self,atributes):
+        self.a1 = atributes[0]
+        self.a2 = atributes[1]
+        self.a3 = atributes[2]
+        self.a4 = atributes[3]
+        self.a5 = atributes[4]
+
+def ask_questions():
+    pass   
+
+db = [1,2,3,4,5]
+domain = ["a","b","c"]
+
+cbr = CBR(db,domain)
+
+if __name__ == '__main__':
+
+    ask_questions()
+    
