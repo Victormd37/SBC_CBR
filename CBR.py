7c45f9701bde6d4eadb264e0cc799dfa51953d93
Victormd37 <victor.molina.diez@estudiantat.upc.edu>
2023-11-28 23:25:37 +0100

    Creat el fitxer .py amb la classe Tree i els seus mètodes Afegits els fitxers csv dels Casos i els Usuarios Modificat el fitxer CBR perque es construeixi l'arbre i carregant les bases de dades dels fitxers .csv

--- CBR.py
+++ CBR.py
@@ -1,10 +1,29 @@
+import pandas as pd
+from index_tree import Tree
+
 class CBR():
 
-    def __init__(self,case_db,domain):
-        self.cases = case_db #Base de Dades?
+    def __init__(self, db, domain, case_db, users_db):
+        self.cases = case_db #Base de Dades? Arbre jerarquic?
+        self.users = users_db #Base de dades d'usuaris
         self.domain = domain #Arbre?
+        self.index_tree = self._build_index_tree()
 
     def retrieve(self, new_case):
+        """
+        New_case: diccionari que conté tots els atributs que composen un cas.
+        Aquest new_case encara no esta a la base de casos pero pot ser que el Usuari si que es trobi a la base d'usuaris.
+
+        En cas que al realitzar les preguntes no s'hagi trobat, es realitzaran les preguntes adients per trobar el perfil d'usuari
+        i s'afegirà SEMPRE a la base d'usuaris.
+        """
+        user_id = new_case['User_id'] # Obtenim user_id del cas nou
+        user = self.users[user_id] # Obtenim la instancia d'usuari guardat a users_db
+
+        similar_cases_id = self.index_tree.buscar_casos(user) #Recuperem els indexos dels n casos més similars
+        # Per els usuaris del cluster 
+            # Crear sets amb info del profile i preferences
+            # cridem self._jaccard_similarity(set1, set2) per calcular la similaritat entre 2 usuaris
         pass
 
     def reuse(self):
@@ -16,17 +35,89 @@ class CBR():
     def retain(self):
         pass
 
-    def _similarity(self,new_case):
-        pass
+    def _build_index_tree(self):
+        """
+        Aquest métode obte les diferents variables i modalitats del perfil d'usuari.
+
+        Retorna un arbre indexat amb tots els casos de la base de casos actual classificats.
+        """
+        char = {}
+        for col in self.users.columns:
+            if col != 'User_id':
+                char[col] = self.users[col].unique()
+        return Tree(char,self.cases,self.users)
+
+
+    def _jaccard_similarity(self,set1, set2):
+        '''
+        En aquest mètode calcularem la metrica de similitud que 
+        utilitzarem per determinar com de similars són dos usuaris. 
+        De moment només utilitzarem el User Profile i el User preferences.
+        Donat que es són vectors de strings utilitzarem Jaccard Similarity.
+        '''
+        
+        intersection = len(set1.intersection(set2))
+        union = len(set1.union(set2))
+        return intersection / union
+
+    def _custom_similarity(self,user1, user2):
+        '''
+        Calculem custom similarity otorgant pesos a aquelles 
+        caracteristiques que considerem més importants. 
+        '''
+        weighted_similarity=0
+        total_weight = 0
+
+        # Listas de profile + preferences (atributos multislot son listas)
+        features1 = user1.get_user_profile()
+        features1.extend(user1.get_user_preferences())
+        features2 = user2.get_user_profile()
+        features2.extend(user2.get_user_preferences())
+
+        # Definimos pesos de los atributos
+        weights = {'Genero':1, 'Edad': 1, 'Vacaciones': 2,
+                    'Genero_literario':3}
+        
+        # Calculamos jaccard similarity con pesos
+        for i, feature in enumerate(weights.items()):
+            if isinstance(features1[i],list): # Cas especial per atributs multislot
+                set1 = set(features1[i])
+                set2 = set(features2[i])
+            else:
+                set1 = set([features1[i]])
+                set2 = set([features2[i]])
+
+            weight = feature[1] # Extraemos el peso de la tupla feature
+            similarity = self._jaccard_similarity(set1, set2)
+            weighted_similarity += weight * similarity
+            total_weight += weight
+
+        # Normalizamos el resultado 
+        if total_weight != 0:
+            weighted_similarity /= total_weight
+
+        return weighted_similarity
+
 
 class User():
 
-    def __init__(self,atributes):
-        self.a1 = atributes[0]
-        self.a2 = atributes[1]
-        self.a3 = atributes[2]
-        self.a4 = atributes[3]
-        self.a5 = atributes[4]
+    def __init__(self,atributes_prof, atributes_pref):
+        self.atributes_prof = atributes_prof
+        self.atributes_pref = atributes_pref
+    
+    def get_user_profile(self):
+        '''
+        Devuelve una lista con los atributos del perfil del usuario.
+        Los atributos multislot seran sublistas.
+        '''
+        return self.atributes_prof
+
+    def get_user_preferences(self):
+        '''
+        Devuelve una lista con los atributos de las preferencias del usuario.
+        Los atributos multislot seran sublistas.
+        '''
+        return self.atributes_pref
 
 class Book():
 
@@ -36,19 +127,71 @@ class Book():
         self.a3 = atributes[2]
         self.a4 = atributes[3]
         self.a5 = atributes[4]
+    
 
 def ask_questions():
     pass   
 
 db = [1,2,3,4,5,6]
-print(db)
+#print(db)
 domain = ["a","b","c","d","k"]
-print(domain)
+#print(domain)
 
+# Definir cases_db amb ids
+# Exemple (tindrà aquesta forma) emplenar a partir de csv drive:
+cases_db = {
+    'Case_id': [1,2,3,4,5,6,7,8],
+    'User_id': [1,1,2,2,3,2,3,4], # Poden repetir-se
+    'Book_id': [0,0,0,0,0,0,0,0], # Poden repetir-se
+    'User_preferences': [0,0,0,0,0,0,0,0],
+    #'Comprado': [] ?
+    'Rating': [0,0,0,0,0,0,0,0], # Valoracions de l'1 al 5 (de moment)
+    #'Timestep': []
+}
 
-cbr = CBR(db,domain)
+cases_db = pd.DataFrame(cases_db)
 
+cases_db = pd.read_csv("Casos.csv")
+
+# Això s'haurà d'emplenar a partir del csv drive, en el cas de users també al ask():
+# Definir users_db (propies instancies usuaris)
+users_db = {
+    'User_id': [1,2,3,4],
+    'Genero' : ['Hombre', 'Mujer', 'Hombre', 'Mujer'],
+    'Edad': ['Adulto','Adulto','Adulto','Adulto'],
+    'Hobbies': ['Playa', 'Montaña', 'Playa', 'Playa']
+}
+
+users_db = pd.DataFrame(users_db)
+
+users_db = pd.read_csv("Usuarios.csv")
+# Definir books_db (propies instancies usuaris)
+
+print(cases_db)
+
+print(users_db)
+
+cbr = CBR(db,domain, cases_db, users_db)
+
+'''
 if __name__ == '__main__':
 
     ask_questions()
-    
+'''
+
+#print(cbr.index_tree.tree.hijos['Hombre'].valores)
+
+#cbr.index_tree.insertar_caso(9,{'Genero': 'Mujer','Edad': 'Adulto','Hobbies': 'Playa'})
+
+#print(cbr.index_tree.tree.hijos['Hombre'].valores)
+#print(cbr.index_tree.tree.hijos['Mujer'].hijos['Playa'].valores)
+
+#print(cbr.index_tree.buscar_casos({'Genero': 'Hombre','Edad': 'Adulto','Hobbies': 'Montaña'}))
+
+print(cbr.index_tree)
+
+#sim_custom = cbr._custom_similarity(user_db.iat[0,1], user_db.iat[3,1])
+#sim_jaccard = cbr._jaccard_similarity(user_db.iat[0,1], user_db.iat[3,1])
+
+#print(sim_custom)
+#print(sim_jaccard)
---
+++ index_tree.py
@@ -0,0 +1,134 @@
+import numpy as np
+
+
+class Tree:
+
+    """
+    Aquesta clase conté un arbre indexat on a les fulles podem trobar els casos que
+    comparteixen atributs similars
+    """
+
+    class TreeNode:
+
+        def __init__(self, valores = [], feature = None, hijos = None):
+            self.feature = feature
+            self.valores = valores
+            self.hijos = hijos
+        
+        def add_valor(self, valor):
+            """Afegeix un nou índex a un node (aquest node será una fulla i l'index el d'un nou cas afegit en la fase de retain)"""
+            self.valores += [valor]
+        def get_casos(self):
+            """Retorna els indexos de tots els casos que es troben en una mateixa fulla"""
+            return self.valores
+        def get_feature(self):
+            """Retorna la característica que utilitza un node per separar els casos"""
+            return self.feature
+        def get_hijo_valor(self, carac):
+            """Retorna el fill que conté els casos amb el valor d'atribut indicat (carac)"""
+            return self.hijos.get(carac)
+    
+    def __init__(self, caracteristicas, casos, users):
+        self.cases = casos
+        self.users = users
+        self.tree = self.crear_arbol(caracteristicas, self.users, self._get_numbercases())
+        
+
+    def crear_arbol(self, caracteristicas, users, n, valor = None):
+        """
+        Aquesta funció és l'encarregada de crear l'arbre indexat a partir dels casos i les característiques
+        del perfil d'usuari.
+
+        Per fer-ho utilitza el métode "_choose_best_partition" per escollir sobre quin atribut partir a cada node.
+        L'arbre es dividirà fins que passi un dels següents casos:
+            - Hi ha 10 o menys casos en un node
+            - No hi ha més característiques per seguir dividint
+            - Només hi ha un usuari que compleix les característiques però apareix en 10 casos o més.
+            - Tots els usuaris que queden tenen les mateixes característiques (seguir dividint només augmenta la mida de l'arbre i les particions serien de n en una branca i 0 casos a l'altra)
+        
+        
+        """
+        if len(caracteristicas) != 0 and users[users.duplicated(subset= users.columns[1:],keep=False)].shape[0] != users.shape[0] and users.shape[0] > 1:
+            if n > 10:
+                best_c = self._choose_best_partition(caracteristicas, users, n)
+                hijos = {}
+                for i in caracteristicas[best_c]:
+                    new_users = users.loc[users[best_c] == i]
+                    l = new_users['Usuario_ID'].tolist()
+                    suma = (self.cases['Usuario_ID'].isin(l)).sum()
+                    c = caracteristicas.copy()
+                    c.pop(best_c)
+                    hijos[f"{i}"] = self.crear_arbol(c, new_users, suma) #Cridem recursivament a la funció
+                arbol = self.TreeNode(valor, best_c, hijos)
+            else:
+                l = users['Usuario_ID'].tolist()
+                casos = self.cases[self.cases['Usuario_ID'].isin(l)]['Caso_ID'].tolist()
+                arbol = self.TreeNode(casos) #Si arribem a un node fulla, aprofitem i afegim directament els indexos dels casos
+        else:
+            l = users['Usuario_ID'].tolist()
+            casos = self.cases[self.cases['Usuario_ID'].isin(l)]['Caso_ID'].tolist()
+            arbol = self.TreeNode(casos) #Si arribem a un node fulla, aprofitem i afegim directament els indexos dels casos
+        return arbol
+    
+    def _choose_best_partition(self, char, users, n):
+        """
+        Aquesta funció escull la característica per la qual el node haurà de dividir les dades.
+        Per fer-ho, simula com quedaria la partició per cada una de les característiques.
+
+        Per escollir la millor, busca aquella característica que reparteix millor els casos.
+            Ex: Si una característica separa els casos en 25 i 25 i una altra ho fa en 15 35, escollirem la primera per fer la partició.
+        
+        Per aconseguir això calculem el valor òptim de les particions que serà: casos en aquell node/ modalitats de la característica.
+        Un cop tenim aquest valor, farem la diferencia entre els casos que queden en cada partició amb el valor òptim:
+            Ex: Amb 50 casos i 2 característiques amb 2 modalitats el valor òptim és 25.
+                Característica 1: Separa en 15 35 els casos.  --->  |25-15| + |25-35|  =  20
+                Característica 2: Separa en 22 28 els casos.  --->  |25-22| + |25-28|  =  6
+
+                Com més petit el valor, millor separa la característica.
+        """
+        best_char = {}
+        for key,values in char.items():
+            if len(values) >1: #Això prevé que si un atribut només té una modalitat per falta de dades al cas inicial no es realitzin particions de més.
+                n_i = n/len(values) #Calculem l'òptim
+                best_char[key] = 0 #Assignem un valor de 0 que anirem incrementant si les particions generades s'allunyen de l'òptim
+                for v in values:
+                    l = users.loc[users[key] == v,['Usuario_ID']]['Usuario_ID'].tolist()
+                    suma = (self.cases['Usuario_ID'].isin(l)).sum()
+                    best_char[key] += abs(n_i - suma) #Calculem el valor absulut entre l'òptim i la partició real.
+                    
+        if len(best_char) > 1: #Això prevé que només hi hagi un atribut disponible per fer el filtratge, ja que no podriem fer el minim.
+            return min(best_char, key = best_char.get)
+        return list(best_char.keys())[0]
+    
+    def _get_numbercases(self):
+        return self.cases.shape[0]
+    
+    def insertar_caso(self, caso_id, user_atr):
+        """Donat l'id del cas que es vol insertar i els atributs de l'usuari d'aquell cas s'inserta en el node corresponent."""
+        nodo = self.tree
+        while not nodo.hijos == None:
+            atr = nodo.get_feature() 
+            nodo = nodo.get_hijo_valor(user_atr[atr])
+        nodo.add_valor(caso_id)
+        
+    
+    def buscar_casos(self, new_case_atr):
+        """Donat els atributs del nou usuari es busquen els casos que es troben al node del arbre que cumpleix les característiques de l'usuari."""
+        nodo = self.tree
+        while not nodo.hijos == None:
+            atr = nodo.get_feature() 
+            nodo = nodo.get_hijo_valor(new_case_atr[atr])
+        return nodo.get_casos()
+    
+    def _crear_dict(self, nodo):
+        """Crea un diccionari de l'arbre per poder-lo imprimir"""
+        if nodo.hijos == None:
+            return nodo.valores
+        diccionario = {'Feature': nodo.feature}
+        for valor,hijo in nodo.hijos.items():
+            diccionario[valor] = self._crear_dict(hijo)
+        return diccionario
+    
+    def __str__(self):
+        """Permet imprimir el arbre com si fos un diccionari, permetent visualitzar de forma més o menys clara el seu contingut"""
+        return str(self._crear_dict(self.tree))
